907ed86c2b47ae1559429ce11c45dfd4e333d470
Your Name <samanbatool08@gmail.com>
2020-03-05 15:31:19 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,37 @@
-export function managePresents(state, action){
-}
+// let state = {
+//     numberOfPresents: 0
+// }
+
+// let action = {
+//     type: "INCREASE"
+// }
+
+// export function managePresents(state, action){
+//     if (action.type === "INCREASE") {
+//         return {numberOfPresents: state.numberOfPresents + 1}
+//     }
+// }
+
+// export function managePresents(state, action){
+//     switch (action.type) {
+//         case "INCREASE" :
+//         return {numberOfPresents: state.numberOfPresents + 1}
+//         default:
+//             return state;
+//     }
+// }
+
+export function managePresents(state = {
+    numberOfPresents: 0,
+  }, action) {
+    switch(action.type) {
+  
+      case "INCREASE":
+        return Object.assign({}, state, {
+          numberOfPresents: state.numberOfPresents + 1
+        });
+  
+      default:
+        return state;
+    }
+  };
